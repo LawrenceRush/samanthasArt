c0fc1aa977197e85dbb534788fc8554448920f1f
Lawrence <l.rush7@gmail.com>
2020-02-26 14:09:45 -0800

    pre samanthV2

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
+const port = 3000
+app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-    res.sendFile('index.html')
+app.get('/paintings', (req, res) => {
+    res.sendFile('public/html/paintings.html' , { root : __dirname});
 });
 
-app.listen(3000, () => console.log('Gator app listening on port 3000!'));
+app.get('/', (req, res) => {
+    res.sendFile('public/html/index.html' , { root : __dirname});
+});
+
+
+app.listen(port, () => console.log(`Example app listening on port http://localhost:${port} !`))
